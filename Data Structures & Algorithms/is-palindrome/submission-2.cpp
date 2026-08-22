b83c0503cbb5f93216629c61937f261b8950f1a0
somaniishika <136316572+somaniishika@users.noreply.github.com>
2026-08-22 11:04:59 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n=s.size();
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            if(!isalnum(s[left])){
+                left++;
+            }
+            else if(!isalnum(s[right])){ 
+                right--;
+            }
+            else if(tolower(s[left])!=(tolower(s[right]))){ 
+                return false;
+            }
+            else{
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+};
