6fe6e69d7a0d47a22204b973dd0dd35ac8218487
somaniishika <136316572+somaniishika@users.noreply.github.com>
2026-08-26 12:25:25 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        int ans=1;
+        for(int i=1;i<=x;i++){
+            if((long long) i*i>x){
+                return ans;
+            }
+            ans=i;
+        }
+        return ans;
+    }
+};
