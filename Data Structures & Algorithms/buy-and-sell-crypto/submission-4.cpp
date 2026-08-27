11feca81fe6e0202e29b170af1a487ddc0c26295
somaniishika <136316572+somaniishika@users.noreply.github.com>
2026-08-27 09:08:06 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int buy=0;
+        int sell=1;
+        int maxprofit=0;
+        while(sell<n){
+            if(prices[sell]>prices[buy]){
+                int profit=prices[sell]-prices[buy];
+                maxprofit=max(maxprofit,profit);
+            }
+            else{
+                buy=sell;
+            }
+            sell++;
+        }
+        return maxprofit;
+    }
+};
