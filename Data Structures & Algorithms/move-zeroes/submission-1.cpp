390322b6f4c18ea7be829006c033624706bd5a05
somaniishika <136316572+somaniishika@users.noreply.github.com>
2026-09-04 14:46:29 +0530

    Add: move-zeroes - submission-1

---
+++ Data Structures & Algorithms/move-zeroes/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        int n=nums.size();
+        for(int j=0;j<n;j++){
+            if(nums[j]!=0){
+                nums[i++]=nums[j];
+            }
+        }
+        while(i<n){
+            nums[i++]=0;
+        }
+    }
+};
